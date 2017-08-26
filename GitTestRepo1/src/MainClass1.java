365caf7b6a3a3dc6a5c2e6c8c18b8a3cc6f9c77c
rakesh <rakslalsangi@gmail.com>
2017-08-26 18:15:23 +0530

    Addedline branch2

--- GitTestRepo1/src/MainClass1.java
+++ GitTestRepo1/src/MainClass1.java
@@ -1,11 +1,6 @@
-
 public class MainClass1 {
-		
-		public static void main(String[] args) {
-			System.out.println("Line1");
-			System.out.println("Line2");
-			System.out.println("Line2");
-			System.out.println("Line3");
-			System.out.println("Line4");
-		}
+
+	public static void main(String[] args) {
+		System.out.println("branch2");
+	}
 }
