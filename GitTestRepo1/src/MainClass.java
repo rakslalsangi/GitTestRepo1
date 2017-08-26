8fc3fe45e1533b5653caae875186a6d66e3890b3
rakesh <rakslalsangi@gmail.com>
2017-08-26 18:34:45 +0530

    Added class

--- GitTestRepo1/src/MainClass.java
+++ GitTestRepo1/src/MainClass.java
@@ -1,6 +1,23 @@
+<<<<<<< Upstream, based on origin/master
 public class MainClass {
 		
 		public static void main(String[] args) {
 			System.out.println("Master");
 		}
 }
+=======
+
+
+
+
+
+
+
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Branch1");
+	}
+}
+>>>>>>> 41f7ac6 Added class
