82fea89d81c3b78ddbcf520b8347328cb1d5ad3b
rakesh <rakslalsangi@gmail.com>
2017-08-26 18:55:35 +0530

    dsadas

--- GitTestRepo1/src/MainClass.java
+++ GitTestRepo1/src/MainClass.java
@@ -1,25 +1,6 @@
-<<<<<<< Upstream, based on origin/master
 public class MainClass {
 		
 		public static void main(String[] args) {
 			System.out.println("Master");
 		}
 }
-=======
-
-
-
-
-
-
-
-
-
-
-public class MainClass {
-	
-	public static void main(String[] args) {
-		System.out.println("Branch1");
-	}
-}
->>>>>>> 41f7ac6 Added class
