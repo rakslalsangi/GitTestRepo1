7e8e368674975cb04d03a3a6c2c6df425cdb8747
rakesh <rakslalsangi@gmail.com>
2017-08-26 18:14:30 +0530

    Added Master line

--- GitTestRepo1/src/MainClass1.java
+++ GitTestRepo1/src/MainClass1.java
@@ -2,10 +2,6 @@
 public class MainClass1 {
 		
 		public static void main(String[] args) {
-			System.out.println("Line1");
-			System.out.println("Line2");
-			System.out.println("Line2");
-			System.out.println("Line3");
-			System.out.println("Line4");
+			System.out.println("Master");
 		}
 }
